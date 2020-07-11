dd40ac0dc9c5f6ac8a7a35d4f2fdae27070db288
OleDakotaJoe <stevendakotasheaves@gmail.com>
2020-07-11 16:32:41 -0500

    backend integration complete

--- swipe-for-tacos/src/components/location-input/location-input.component.js
+++ swipe-for-tacos/src/components/location-input/location-input.component.js
@@ -13,17 +13,30 @@ class LocationInputPage extends React.Component {
             APIkey: 'AIzaSyALcnLOO6jrbkQD5JL7tlMjFfb4EmC4W24',
             keywords:'',
             guid: null,
-            radius: 5000
+            radius: 5000,
+            restaurantArray: [],
+            serverUrl: 'http://localhost:3000'
         }
     }
 
+    handleChange = ( event ) => {
+        //dynamically sets the state given an changeEvent
+        const {name,value} = event.target
+        this.setState({[name]: value})
+    }
+
+    handleSubmit = (event) => {
+        
+        event.preventDefault();
+        this.convertToLatLng()
+        
+    }
 
     convertToLatLng = () => {
         //This code takes a zip code input from the user, and converts it to latitude an longitude for the application to use in its fetch request
         fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.zip}&key=${this.state.APIkey}`)
             .then(res => res.json())
             .then(data => this.setState({lat:data.results[0].geometry.location.lat,lng:data.results[0].geometry.location.lng}
-                //,()=>console.log(this.state.lat +','+ this.state.lng)
                 ,()=> this.fetchRestaurants()
                 ))
             .catch(err => console.log(err))
@@ -34,21 +47,26 @@ class LocationInputPage extends React.Component {
         //takes latitude, longitude, radius, type(restaurant), and apiKey from state and inserts it into fetch(get request) which returns json object with resaurant data
         fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.state.lat},${this.state.lng}1&radius=${this.state.radius}&type=${this.state.type}&key=${this.state.APIkey}`)
             .then(res => res.json())
-            .then(data => console.log(data.results))
+            .then(data => this.handleRestaurantArray(data))
             .catch(err => console.log(err))
+    }  
+
+    handleRestaurantArray = (data) => {
+        let arr = data.results.map(result => ({name: result.name, rating: null}) )
+        this.setState({restaurantArray: arr}, ()=> this.sendRestaurantsArrayToBackend())
     }
 
-    handleSubmit = (event) => {
-        
-        event.preventDefault();
-        this.convertToLatLng()
-        
-    }
-
-    handleChange = ( event ) => {
-        //dynamically sets the state given an changeEvent
-        const {name,value} = event.target
-        this.setState({[name]: value})
+    sendRestaurantsArrayToBackend = () => {
+        fetch(`${this.state.serverUrl}/123456`,{
+            mode: 'cors',
+            method: 'POST',
+            body: JSON.stringify(this.state.restaurantArray),
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
     }
 
     render() {
